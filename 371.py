97b7252e8051d98760ce5d2bb2800d532bad40ef
이정규 <kue411@naver.com>
2022-11-01 15:43:53 +0900

    221101

--- 371.py
+++ 371.py
@@ -11,12 +11,20 @@ class Solution(object):
             carry = ((a&b)<<1) & mask
             a = sums
             b = carry
-            print(a)
-            print(b)
+            #print('mask')
+            #print(bin(mask))
+            #print('sums')
+            #print(a, bin(a))
+            #print('carry')
+            #print(b, bin(b))
         
-        if (a>>31) & 1: # If a is negative in 32 bits sense
+        if a > 0x7fffffff:
+            #print('~(a^mask)')
+            #print(~(a^mask))
             return ~(a^mask)
         
+        #print('ans')
+        #print(a)
         return a
 
         # https://leetcode.com/problems/sum-of-two-integers/discuss/776952/python-best-leetcode-371-explanation-for-python
