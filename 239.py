484996a26473f2e447c38d4a30598505fc611f99
이정규 <kue411@naver.com>
2022-11-07 10:10:22 +0900

    221107

--- 239.py
+++ 239.py
@@ -6,7 +6,7 @@ class Solution(object):
         :rtype: List[int]
         """
         
-        que = []
+        que = collections.deque()
         i = j = 0
         ans = []
         while j < len(nums):
@@ -26,40 +26,9 @@ class Solution(object):
                 
                 if nums[i] == que[0]:
                     
-                    que.pop(0)
+                    que.popleft()
                     
                 i += 1
                 j += 1
                 
         return ans
-
-# class Solution(object):
-#     def maxSlidingWindow(self, nums, k):
-#         """
-#         :type nums: List[int]
-#         :type k: int
-#         :rtype: List[int]
-#         """
-#         max_temp = float('-inf')                # -(10 ** 4)로 했는데 시간초과남
-#         window = collections.deque()            # 슬라이싱으로 3개 채우고 시작하려고 했는데 시간초과남
-#         res = []
-        
-#         for i, val in enumerate(nums):
-#             window.append(val)
-            
-#             if i < k - 1:
-#                 continue
-            
-#             if max_temp == float('-inf'):
-#                 max_temp = max(window)
-#             elif max_temp < val:
-#                 max_temp = val
-                
-#             res.append(max_temp)
-            
-#             if max_temp == window.popleft():
-#                 max_temp = float('-inf')
-                
-        
-#         return res
-        
